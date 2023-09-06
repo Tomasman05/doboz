5755eb0512b266e4733a5462585ba6e349e4085f
Vito <lolozol80xd@gmail.com>
2023-09-06 11:32:03 +0200

    start

---
+++ src/app/doboz/doboz.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doboz',
+  templateUrl: './doboz.component.html',
+  styleUrls: ['./doboz.component.css']
+})
+export class DobozComponent {
+
+}
